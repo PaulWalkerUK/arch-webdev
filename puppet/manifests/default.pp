6ef5e6dce41d55ac6773a20672e6a5a8f6648988
PaulWalkerUK <walker.paul+github@gmail.com>
2020-06-06 10:51:31 +0100

    Enable PHP extension 'intl'

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -19,11 +19,19 @@ package { 'nano': ensure => installed }
 package { 'php-intl': ensure => installed }
 package { 'firefox': ensure => installed }
 package { 'composer': ensure => installed }
+package { 'mousepad': ensure => installed }
 
 exec { 'flyway':
-        command => 'cd /tmp; git clone https://aur.archlinux.org/flyway.git; cd flyway; makepkg -si --noconfirm',
-        provider => 'shell',
-        user => 'vagrant',
-        require => Package['base-devel','git'],
-        unless => 'which flyway',
+  command => 'cd /tmp; git clone https://aur.archlinux.org/flyway.git; cd flyway; makepkg -si --noconfirm',
+  provider => 'shell',
+  user => 'vagrant',
+  require => Package['base-devel','git'],
+  unless => 'which flyway',
+  timeout => 0,
+}
+
+file { '/etc/php/conf.d/10_enable_intl.ini':
+  ensure => file,
+  content => "extension=intl",
+  require => Package['php-intl'],
 }
